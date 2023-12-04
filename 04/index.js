c2d0ef8bffaae9f4e0ed9546f01c1eb11c2150cd
Oriol Martí <omarti@theknotww.com>
2023-12-04 10:16:56 +0100

    Improve timing for script day 4

--- 04/index.js
+++ 04/index.js
@@ -4,26 +4,26 @@ const timer = new Timer()
 
 const input = getInput(4)
 
-const numbersGrid = input.map((row, i) => {
+const grid = input.map((row) => {
     const [inputPart1, inputPart2] = row.split('|')
     const numbersPart = inputPart1.split(':')
     const gameId = Number(numbersPart[0].trim().split(' ').filter(v => v !== '')[1])
 
-    const playingNumbers = numbersPart[1].trim().split(' ').map(v => Number(v)).filter(v => v > 0)
-    const winningNumbers = inputPart2.trim().split(' ').map(v => Number(v)).filter(v => v > 0)
+    const playingNumbers = numbersPart[1].split(' ').map(v => Number(v)).filter(v => v > 0)
+    const winningNumbers = inputPart2.split(' ').map(v => Number(v)).filter(v => v > 0)
+
+    const totalWins = winningNumbers.filter(n => playingNumbers.indexOf(n) !== -1).length
 
     return {
         gameId,
-        winningNumbers,
-        playingNumbers
+        totalWins,
     }
 })
 
 function part1() {
     let result = 0
 
-    numbersGrid.forEach(({ winningNumbers, playingNumbers }) => {
-        const totalWins = getTotalWins(winningNumbers, playingNumbers)
+    grid.forEach(({ totalWins }) => {
         let currentResult = totalWins > 0 ? 1 : 0
         for (let i = 2; i <= totalWins; i++) {
             currentResult *= 2
@@ -35,15 +35,7 @@ function part1() {
     return result
 }
 
-function getTotalWins(winningNumbers, playingNumbers) {
-    const filteredNumbers = winningNumbers.filter(function (n) {
-        return playingNumbers.indexOf(n) !== -1;
-    })
-
-    return filteredNumbers.length
-}
-
-function part2(appenededGames, result) {
+function part2(appenededGames, result = 0) {
     if (appenededGames.length === 0) {
         return result
     }
@@ -51,25 +43,21 @@ function part2(appenededGames, result) {
     result += appenededGames.length
 
     const newAppenededGames = []
-    appenededGames.forEach(({ gameId, winningNumbers, playingNumbers }) => {
-        const totalWins = getTotalWins(winningNumbers, playingNumbers)
+
+    for (const { gameId, totalWins } of appenededGames) {
         for (let i = 1; i <= totalWins; i++) {
-            const appendedGame = numbersGrid.find(v => v.gameId === gameId + i)
+            const appendedGame = grid[gameId + i - 1]
             if (appendedGame) {
                 newAppenededGames.push(appendedGame)
             }
         }
-    })
+    }
 
     return part2(newAppenededGames, result)
 }
 
-function getResult(isSecondPart = false) {
-    if (isSecondPart) {
-        return part2(numbersGrid, 0);
-    }
-
-    return part1()
+function getResult(isFirstPart = true) {
+    return isFirstPart ? part1() : part2(grid)
 }
 
-export default returnResult(getResult(), getResult(true), timer)
+export default returnResult(getResult(), getResult(false), timer)
--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const timer = new Timer()
 const folders = fs.readdirSync('./', { recursive: true })
     .filter(v => v.search('index.js') !== -1 && v !== 'index.js')
 
-console.log('Get folders', timer.getDuration())
 const promises = folders.map(async (folder) => {
     const day = folder.split('/')[0]
     const { default: { result_1, result_2, duration } } = await import(`./${folder}`)
@@ -13,7 +12,6 @@ const promises = folders.map(async (folder) => {
 })
 
 const results = await Promise.all(promises)
-console.log('Resolve Promises', timer.getDuration())
 
 console.table(results.reduce((acc, { day, ...x }) => { acc[day] = x; return acc }, {}))
-console.log('Final Result', timer.getDuration())
+console.log(`Execution time: ${timer.getDuration()}s`)
